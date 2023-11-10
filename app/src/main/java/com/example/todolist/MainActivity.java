2eb51bf11961297368d6a2f37843fb5ceb5e1e68
nuetka <anjalichtin@mail.ru>
2023-11-10 22:58:35 +0700

    Добавлено листание по дням

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -5,18 +5,28 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.todolist.Adapter.ToDoAdapter;
 import com.example.todolist.Model.ToDoModel;
 import com.example.todolist.Utils.DataBaseHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements OnDialogCloseListener {
 
@@ -26,10 +36,54 @@ public class MainActivity extends AppCompatActivity implements OnDialogCloseList
     private List<ToDoModel> mList;
     private ToDoAdapter adapter;
 
+    TextView dateTextView;
+    ImageView calendarImage;
+    Calendar selectedDate = Calendar.getInstance();
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+
+    ImageButton previousButton;
+    ImageButton nextButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateTextView = (TextView) findViewById(R.id.textview);
+        previousButton = (ImageButton) findViewById(R.id.previousButton);
+        nextButton = (ImageButton) findViewById(R.id.nextButton);
+        calendarImage = (ImageView) findViewById(R.id.calendarImage);
+
+        // установка текущей даты
+        String currentDate = sdf.format(new Date());
+        dateTextView.setText(currentDate);
+
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedDate.add(Calendar.DAY_OF_MONTH, -1);
+                updateDateTextView();
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedDate.add(Calendar.DAY_OF_MONTH, 1);
+                updateDateTextView();
+            }
+        });
+
+
+        calendarImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(MainActivity.this, date, selectedDate.get(Calendar.YEAR), selectedDate.get(Calendar.MONTH), selectedDate.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
 
         mRecyclerview = findViewById(R.id.recyclerview);
         fab = findViewById(R.id.fab);
@@ -54,6 +108,39 @@ public class MainActivity extends AppCompatActivity implements OnDialogCloseList
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RecyclerViewTouchHelper(adapter));
         itemTouchHelper.attachToRecyclerView(mRecyclerview);
     }
+
+    private void updateDateTextView() {
+        String selectedDateString = sdf.format(selectedDate.getTime());
+        dateTextView.setText(selectedDateString);
+
+        // Check if the selected date is today and set the color of the text accordingly
+        Date today = new Date();
+        if(sdf.format(today).equals(selectedDateString)){
+            dateTextView.setTextColor(Color.BLACK);
+        } else {
+            dateTextView.setTextColor(Color.GRAY);
+        }
+    }
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            selectedDate.set(Calendar.YEAR, year);
+            selectedDate.set(Calendar.MONTH, monthOfYear);
+            selectedDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+            String selectedDateString = sdf.format(selectedDate.getTime());
+            dateTextView.setText(selectedDateString);
+
+            // Check if the selected date is today and set the color of the text accordingly
+            Date today = new Date();
+            if(sdf.format(today).equals(selectedDateString)){
+                dateTextView.setTextColor(Color.BLACK);
+            } else {
+                dateTextView.setTextColor(Color.GRAY);
+            }
+        }
+    };
+
+
 
     @Override
     public void onDialogClose(DialogInterface dialogInterface) {
